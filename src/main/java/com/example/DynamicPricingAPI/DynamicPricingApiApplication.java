019e6d22c26fbf727ed5674acfebd28249850a44
Eghosa Edoghogho <eghosa@Eghosas-MacBook-Air.local>
2024-11-02 23:59:08 +0000

    Initial commit - Set up Spring Boot project with Product entity

---
+++ src/main/java/com/example/DynamicPricingAPI/DynamicPricingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.DynamicPricingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPricingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPricingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DynamicPricingAPI/DynamicPricingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DynamicPricingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPricingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
