46b41f81bdca3e16d13fc2e794893dbeba8d9d98
eghosa <eghosa40@gmail.com>
2025-01-04 20:53:10 +0000

    test

--- src/main/java/com/example/DynamicPricingAPI/Service/ProductService.java
+++ src/main/java/com/example/DynamicPricingAPI/Service/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ProductService {
     Product createProduct(Product product);
-    Product getProductById(Long id);
+    Product getProductById(Long id); //test
     List<Product> getAllProducts();
     Product updateProduct(Long id, Product product);
     void deleteProduct(Long id);
